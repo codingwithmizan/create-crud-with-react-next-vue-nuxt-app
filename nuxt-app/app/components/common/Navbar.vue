4a234358c2e78123049e87b9102ffeba622e4748
codingwithmizan <mizan.samb@gmail.com>
2024-10-26 11:55:25 +0600

    add styles

--- nuxt-app/app/components/common/Navbar.vue
+++ nuxt-app/app/components/common/Navbar.vue
@@ -19,7 +19,7 @@ const navLinks = [
 <template>
   <nav class="h-20 border flex items-center shadow">
     <div class="pl-10 uppercase font-semibold !text-green-700">nuxt app</div>
-    <div class="flex justify-between w-full">
+    <div class="flex justify-between w-full pr-10">
       <ul class="flex gap-6 pl-16">
         <li v-for="link in navLinks" :key="link.id">
           <NuxtLink :to="localePath(link.path)">{{ link.label }}</NuxtLink>
